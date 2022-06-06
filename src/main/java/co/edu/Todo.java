4d4fddf1b556c3ae214ac9f98a254e811a230782
JInhyoung Kim <jinhyoung0213@gmail.com>
2022-06-06 22:23:07 +0900

    not mine

---
+++ src/main/java/co/edu/Todo.java
@@ -0,0 +1,29 @@
+package co.edu;
+
+public class Todo {
+	private int id;
+	private String content;
+	private String status;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	public String getStatus() {
+		return status;
+	}
+	public void setStatus(String status) {
+		this.status = status;
+	}
+	
+
+
+}
